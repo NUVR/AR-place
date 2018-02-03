dbefabe32d3c9993eb3ad818208909204b44e101
DarkAce65 <tvdood99@gmail.com>
2018-02-03 18:37:41 -0500

    Convert tabs to spaces

--- src/index.js
+++ src/index.js
@@ -6,8 +6,8 @@ const rootEl = document.querySelector('#root');
 ReactDOM.render(<App />, rootEl);
 
 if(module.hot) {
-	module.hot.accept('./layouts/App', function() {
-		const NextApp = require('./layouts/App').default;
-		ReactDOM.render(<NextApp />, rootEl);
-	});
+    module.hot.accept('./layouts/App', function() {
+        const NextApp = require('./layouts/App').default;
+        ReactDOM.render(<NextApp />, rootEl);
+    });
 }
--- src/layouts/App.js
+++ src/layouts/App.js
@@ -3,9 +3,9 @@ import React from 'react';
 import Home from '../components/Home';
 
 export default class App extends React.Component {
-	render() {
-		return (
-			<Home />
-		);
-	}
+    render() {
+        return (
+            <Home />
+        );
+    }
 }
