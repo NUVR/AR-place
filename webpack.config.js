b0cadac766db18f7528589bf78469931ca0e1801
DarkAce65 <tvdood99@gmail.com>
2018-02-28 21:25:49 -0500

    Add jsartoolkit5 as submodule

--- webpack.config.js
+++ webpack.config.js
@@ -27,7 +27,7 @@ module.exports = {
         rules: [
             {
                 test: /\.(jsx?)$/,
-                exclude: /node_modules/,
+                exclude: [/node_modules/, /lib/],
                 use: [
                     {
                         loader: 'babel-loader',
