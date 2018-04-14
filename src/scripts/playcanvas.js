a5fdabc36a6f02a53a3f1dac0428c725f7b4ea0d
Abbondanzo <peter@abbondanzo.com>
2018-04-14 02:19:53 -0700

    Moving to ViroReact

--- src/scripts/playcanvas.js
+++
@@ -1,9 +0,0 @@
-function init() {
-    const t = performance.now();
-    return import('../../lib/playcanvas').then(() => {
-        /*eslint no-console: "off"*/
-        console.log('loaded playcanvas in ' + Math.round(performance.now() - t) + 'ms');
-    });
-}
-
-export default init;
