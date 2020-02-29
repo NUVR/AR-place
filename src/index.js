44e8f8fee55082bd811a25741e7c57348ac11ccf
DarkAce65 <tvdood99@gmail.com>
2020-02-29 16:25:43 -0500

    Initialize via button click

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ARManager from './ARManager';
 import './style.css';
 
 document.addEventListener('DOMContentLoaded', () => {
-  ARManager.init();
-  ARManager.startRendering();
+  document.querySelector('#start').addEventListener('click', ev => {
+    ARManager.init();
+    ARManager.startRendering();
+    ev.target.classList.add('hidden');
+  });
 });
